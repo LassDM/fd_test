dac69d97bcb0b748c0feb91debe38f00c474b429
Дмитрий Лазарев <lass@salerman.ru>
2022-04-29 14:01:17 +0300

    Промежуточный коммит - сохраняем состояние. Добавлен сид для пользователей.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use App\Models\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -36,6 +38,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 
     /**
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,19 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            CarSeeder::class,
-            DriverSeeder::class,
-            UserSeeder::class
-        ]);
+        \App\Models\User::factory()->count(2)->create();
     }
 }
